4b09ba719218ecc43ef39423206d87148b5f976c
Mafuyu404 <404@mafuyu.link>
2025-07-27 02:31:46 +0800

    灵活按键禁用以及Utils拆分

---
+++ src/main/java/com/mafuyu404/smartkeyprompts/util/PromptUtils.java
@@ -0,0 +1,22 @@
+package com.mafuyu404.smartkeyprompts.util;
+
+import com.mafuyu404.smartkeyprompts.init.KeyPrompt;
+import com.mafuyu404.smartkeyprompts.init.Utils;
+
+public class PromptUtils {
+    public static void show(String id, String desc) {
+        addDesc(desc).toGroup(id);
+    }
+
+    public static void custom(String id, String key, String desc) {
+        addDesc(desc).forKey(key).withCustom(true).toGroup(id);
+    }
+
+    public static void alias(String id, String desc, String alias) {
+        addDesc(desc).withKeyAlias(alias).toGroup(id);
+    }
+
+    public static KeyPrompt addDesc(String desc) {
+        return new KeyPrompt("", Utils.getKeyByDesc(desc), desc, false);
+    }
+}
